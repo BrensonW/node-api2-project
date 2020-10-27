dda6db4cabe46a61eb14a2b860956e4243fe6e97
Brenson <brensonw300@gmail.com>
2020-10-27 09:02:40 -0700

    Changed some code up in db.js and posts-router.js it all looks good

--- data/db.js
+++ data/db.js
@@ -1,7 +1,6 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
-
 module.exports = {
   find,
   findById,
@@ -12,44 +11,38 @@ module.exports = {
   findCommentById,
   insertComment,
 };
-
 function find() {
   return db('posts');
 }
-
 function findById(id) {
   return db('posts').where({ id: Number(id) });
 }
-
 function insert(post) {
   return db('posts')
     .insert(post, 'id')
     .then(ids => ({ id: ids[0] }));
 }
-
 function update(id, post) {
   return db('posts')
     .where('id', Number(id))
     .update(post);
 }
-
 function remove(id) {
   return db('posts')
     .where('id', Number(id))
     .del();
 }
-
 function findPostComments(postId) {
   return db('comments')
     .join('posts', 'posts.id', 'post_id')
-    .select('comments.*', 'title as post')
+    .select('text as comment', 'title as post')
     .where('post_id', postId);
 }
 
 function findCommentById(id) {
   return db('comments')
     .join('posts', 'posts.id', 'post_id')
-    .select('comments.*', 'title as post')
+    .select('text as comment', 'title as post')
     .where('comments.id', id);
 }
 
