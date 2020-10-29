b9fa5a2ccaf7b5b963fcde0b97beb985dbf5371c
Brenson <brensonw300@gmail.com>
2020-10-29 11:17:23 -0700

    adding env to deploy

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// require('dotenv').config()
+ require('dotenv').config()
 const express = require('express')
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const posts = require('./posts/posts-router')
 const server = express();
